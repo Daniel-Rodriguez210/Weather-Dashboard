4bdbf3d69485ba5a24507af7b165114073bef7d8
Daniel Rodriguez <drod18@hotmail.com>
2020-11-26 15:06:22 -0600

    cleaning up the forecast code

--- assets/script.js
+++ assets/script.js
@@ -12,12 +12,12 @@ $.ajax({
     console.log(response);
  
 
-  $(".city").html("<h5>" + response.name + " </h5>");
+  $(".city").html("<h4>" + response.name + " </h4>");
   $(".humidity").text("Humidity: " + response.main.humidity + " %");
   $(".wind").text("Wind Speed: " + response.wind.speed + " MPH");
   
   var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-  $(".tempF").text("Temperature (F) " + tempF.toFixed(0) + " °F");
+  $(".tempF").text("Temperature: " + tempF.toFixed(0) + " °F");
 
   var lat = response.coord.lat;
   var lon = response.coord.lon;
@@ -40,30 +40,30 @@ $.ajax({
 }).then(function(response) {
     console.log(response)
 
-    $(".date1").text(response.list[0].dt_txt)
-    var tempF1 = (response.list[0].main.temp-273.15) * 1.80 + 32;
-    $(".tempF1").text("Temperature (F) " + tempF1.toFixed(0) + " °F");
-    $(".humid1").text("Humidity: " + response.list[0].main.humidity + " %");
+    $(".date1").text(response.list[3].dt_txt)
+    var tempF1 = (response.list[3].main.temp-273.15) * 1.80 + 32;
+    $(".tempF1").text("Temperature: " + tempF1.toFixed(0) + " °F");
+    $(".humid1").text("Humidity: " + response.list[3].main.humidity + " %");
 
-    $(".date2").text(response.list[1].dt_txt)
-    var tempF2 = (response.list[1].main.temp-273.15) * 1.80 + 32;
-    $(".tempF2").text("Temperature (F) " + tempF2.toFixed(0) + " °F");
-    $(".humid2").text("Humidity: " + response.list[1].main.humidity + " %");
+    $(".date2").text(response.list[11].dt_txt)
+    var tempF2 = (response.list[11].main.temp-273.15) * 1.80 + 32;
+    $(".tempF2").text("Temperature: " + tempF2.toFixed(0) + " °F");
+    $(".humid2").text("Humidity: " + response.list[11].main.humidity + " %");
 
-    $(".date3").text(response.list[2].dt_txt)
-    var tempF3 = (response.list[2].main.temp-273.15) * 1.80 + 32;
-    $(".tempF3").text("Temperature (F) " + tempF3.toFixed(0) + " °F");
-    $(".humid3").text("Humidity: " + response.list[2].main.humidity + " %");
+    $(".date3").text(response.list[19].dt_txt)
+    var tempF3 = (response.list[19].main.temp-273.15) * 1.80 + 32;
+    $(".tempF3").text("Temperature:  " + tempF3.toFixed(0) + " °F");
+    $(".humid3").text("Humidity: " + response.list[19].main.humidity + " %");
 
-    $(".date4").text(response.list[3].dt_txt)
-    var tempF4 = (response.list[3].main.temp-273.15) * 1.80 + 32;
-    $(".tempF4").text("Temperature (F) " + tempF4.toFixed(0) + " °F");
-    $(".humid4").text("Humidity: " + response.list[3].main.humidity + " %");
+    $(".date4").text(response.list[27].dt_txt)
+    var tempF4 = (response.list[27].main.temp-273.15) * 1.80 + 32;
+    $(".tempF4").text("Temperature: " + tempF4.toFixed(0) + " °F");
+    $(".humid4").text("Humidity: " + response.list[27].main.humidity + " %");
 
-    $(".date5").text(response.list[4].dt_txt)
-    var tempF5 = (response.list[4].main.temp-273.15) * 1.80 + 32;
-    $(".tempF5").text("Temperature (F) " + tempF5.toFixed(0) + " °F");
-    $(".humid5").text("Humidity: " + response.list[4].main.humidity + " %");
+    $(".date5").text(response.list[35].dt_txt)
+    var tempF5 = (response.list[35].main.temp-273.15) * 1.80 + 32;
+    $(".tempF5").text("Temperature: " + tempF5.toFixed(0) + " °F");
+    $(".humid5").text("Humidity: " + response.list[35].main.humidity + " %");
 
 
 });
